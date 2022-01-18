a64b7bb448ccbec3a3be63c2da3dc97f6d205e4a
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-18 20:27:14 +0530

    updated

---
+++ Steps.md
@@ -0,0 +1,6 @@
+```
+$ npx create-react-app ./ [Delete the 'src' folder and replace with another]
+$ npm install react-icons [Dependency to use icons]
+
+
+```
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+          <h1>Joy Ma</h1>
+        </div>
+    )
+}
+
+export default App;
