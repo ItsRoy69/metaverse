e89949c66e5047df48e7bdb173a870a9409b38fd
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-19 22:15:03 +0530

    features

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -14,8 +14,8 @@ const WhatGPT3 = () => {
           </div>
           <div className="gpt3__whatgpt3-container">
             <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-            <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-            <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+            <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+            <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
           </div>
         </div>
     )
