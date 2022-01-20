a9b9c028a7fb32e4d993d0f87e651ad930728ba5
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-20 09:25:31 +0530

    article and blog

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logos.png';
 import './Navbar.css';
 
 // We are using BEM -> Block Element Modifier for naming the classname
@@ -10,7 +10,7 @@ return (
   //wrapping all the writings of the navbar button inside "Menu"
   <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#wgpt3">What is GPT3?</a></p>
+    <p><a href="#wgpt3">What is METAVERSE?</a></p>
     <p><a href="#possibility">Open AI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
